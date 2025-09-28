ddfc1a55824105f1a7eaa3b13d118f770a689a0a
Juliano Santos <julianosantosdev@outlook.com>
2025-09-27 22:22:09 -0300

    fix: import function name

--- test/test.calc.py
+++ test/test.calc.py
@@ -2,10 +2,10 @@ import pytest
 from src.calc import add, divide
 
 def test_somar():
-    assert somar(2, 3) == 5
-    assert somar(-1, 1) == 0
+    assert add(2, 3) == 5
+    assert add(-1, 1) == 0
     
 def test_dividir():
-    assert dividir(10, 2) == 5
+    assert divide(10, 2) == 5
     with pytest.raises(ValueError):
-        dividir(10, 0)
+        divide(10, 0)
