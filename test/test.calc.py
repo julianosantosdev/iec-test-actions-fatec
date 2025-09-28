fe6a43a10250a7be3ab1a2a94e8e47f92e0988d4
Juliano Santos <julianosantosdev@outlook.com>
2025-09-27 22:08:35 -0300

    feat: add tests

---
+++ test/test.calc.py
@@ -0,0 +1,11 @@
+import pytest
+from src.calc import sum, divide
+
+def test_somar():
+    assert somar(2, 3) == 5
+    assert somar(-1, 1) == 0
+    
+def test_dividir():
+    assert dividir(10, 2) == 5
+    with pytest.raises(ValueError):
+        dividir(10, 0)
